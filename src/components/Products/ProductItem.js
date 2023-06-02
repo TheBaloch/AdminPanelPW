20dd27aab60b621d685ff65d69de1de620268c62
TheBaloch <abdullahbaloch797@gmail.com>
2023-06-02 16:16:42 +0500

    Products Final

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function ProductItem({ product, onDelete }) {
+function ProductItem({ product, onDelete, onUpdateProduct }) {
   const imgURL = `http://localhost:5000${product.image}`;
+  const [showModal, setShowModal] = useState(false);
+  const [editedProduct, setEditedProduct] = useState({
+    name: product.name,
+    price: product.price,
+    description: product.description,
+    image: null,
+  });
 
   const handleEdit = () => {
-    alert('Edit');
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setEditedProduct((prevProduct) => ({
+      ...prevProduct,
+      image: file,
+    }));
+  };
+
+  const handleProductEdit = async () => {
+    const formData = new FormData();
+    formData.append('name', editedProduct.name);
+    formData.append('price', editedProduct.price);
+    formData.append('description', editedProduct.description);
+    formData.append('image', editedProduct.image);
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/products/${product._id}`,
+        {
+          method: 'PUT',
+          body: formData,
+        }
+      );
+
+      if (response.ok) {
+        console.log('Product updated successfully');
+        handleClose();
+        onUpdateProduct();
+      } else {
+        console.error('Error updating product:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error updating product:', error);
+    }
   };
 
   const handleDelete = () => {
@@ -22,6 +80,70 @@ function ProductItem({ product, onDelete }) {
       <h6>{product.price}</h6>
       <button onClick={handleEdit}>Edit</button>
       <button onClick={handleDelete}>Delete</button>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={editedProduct.name}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="text"
+                name="price"
+                value={editedProduct.price}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="description"
+                value={editedProduct.description}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formImage">
+              <Form.Label>Current Image</Form.Label>
+              <br />
+              <img
+                src={imgURL}
+                alt={product.name}
+                style={{
+                  height: '100px',
+                  width: '100px',
+                  marginBottom: '10px',
+                }}
+              />
+              <Form.Label>Upload New Image</Form.Label>
+              <Form.Control
+                type="file"
+                name="image"
+                onChange={handleImageChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleProductEdit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductItem from './ProductItem';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    description: '',
+    image: null, // Added image field
+  });
 
   const fetchProducts = async () => {
     try {
@@ -19,7 +29,56 @@ function Products() {
   }, []);
 
   const handleAdd = () => {
-    alert('Add New');
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setNewProduct({
+      name: '',
+      price: '',
+      description: '',
+      image: null,
+    });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setNewProduct((prevProduct) => ({
+      ...prevProduct,
+      image: file,
+    }));
+  };
+
+  const handleProductSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append('name', newProduct.name);
+      formData.append('price', newProduct.price);
+      formData.append('description', newProduct.description);
+      formData.append('image', newProduct.image);
+
+      await axios.post('http://localhost:5000/api/products', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      handleClose();
+      fetchProducts(); // Fetch the updated list of products after successful addition
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
   };
 
   const handleProductDelete = async (productId) => {
@@ -36,6 +95,10 @@ function Products() {
     }
   };
 
+  const updateProductList = () => {
+    fetchProducts();
+  };
+
   return (
     <div style={{ width: '30rem' }}>
       <button onClick={handleAdd}>Add New</button>
@@ -45,8 +108,57 @@ function Products() {
           key={product._id}
           product={product}
           onDelete={handleProductDelete}
+          onUpdateProduct={updateProductList}
         />
       ))}
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleProductSubmit}>
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={newProduct.name}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="text"
+                name="price"
+                value={newProduct.price}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="description"
+                value={newProduct.description}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formImage">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="file"
+                name="image"
+                onChange={handleImageChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
