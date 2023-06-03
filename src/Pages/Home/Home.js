faeced53672c6739bdd75e942b6c9f2aff1808f1
GoharHayyat <goharchisthi@gmail.com>
2023-06-03 22:04:03 +0500

    c

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,72 @@
-import React, { useState } from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import Orders from '../../components/Orders/Orders';
-import DayCare from '../../components/DayCare/DayCare';
-import Users from '../../components/Users/Users';
-import Products from '../../components/Products/Products';
-import Insurance from '../../components/Insurance/Insurance';
-import './Home.css';
-import Doctors from '../../components/Doctors/Doctors';
+// import React, { useState } from 'react';
+// import Sidebar from '../../components/Sidebar/Sidebar';
+// import Orders from '../../components/Orders/Orders';
+// import DayCare from '../../components/DayCare/DayCare';
+// import Users from '../../components/Users/Users';
+// import Products from '../../components/Products/Products';
+// import Insurance from '../../components/Insurance/Insurance';
+// import './Home.css';
+// import Doctors from '../../components/Doctors/Doctors';
+
+// const MainHome = () => {
+//   const [selectedComponent, setSelectedComponent] = useState('products');
+
+//   // Function to handle sidebar selection
+//   const handleSidebarSelection = (component) => {
+//     setSelectedComponent(component);
+//   };
+
+//   // Function to render the selected component
+//   const renderSelectedComponent = () => {
+//     switch (selectedComponent) {
+//       case 'products':
+//         return <Products />;
+//       case 'users':
+//         return <Users />;
+//       case 'doctors':
+//         return <Doctors />;
+//       case 'daycare':
+//         return <DayCare />;
+//       case 'insurance':
+//         return <Insurance />;
+//       case 'orders':
+//         return <Orders />;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   return (
+//     <>
+//       {/* <Sidebar handleSelection={handleSidebarSelection} /> */}
+//       <div style={{ height: '100%' }} className="container-fluid">
+//         <div style={{ height: '100%' }} className="row">
+//           <div style={{ width: '16%' }}>
+//             <Sidebar handleSelection={handleSidebarSelection} />
+//           </div>
+//           <div style={{ width: '84%', background: '#f9fafe' }}>
+//             <div className="content">{renderSelectedComponent()}</div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MainHome;
+
+import React, { useState } from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Orders from "../../components/Orders/Orders";
+import DayCare from "../../components/DayCare/DayCare";
+import Users from "../../components/Users/Users";
+import Products from "../../components/Products/Products";
+import Insurance from "../../components/Insurance/Insurance";
+import "./Home.css";
+import Doctors from "../../components/Doctors/Doctors";
 
 const MainHome = () => {
-  const [selectedComponent, setSelectedComponent] = useState('products');
+  const [selectedComponent, setSelectedComponent] = useState("products");
 
   // Function to handle sidebar selection
   const handleSidebarSelection = (component) => {
@@ -19,17 +76,17 @@ const MainHome = () => {
   // Function to render the selected component
   const renderSelectedComponent = () => {
     switch (selectedComponent) {
-      case 'products':
+      case "products":
         return <Products />;
-      case 'users':
+      case "users":
         return <Users />;
-      case 'doctors':
+      case "doctors":
         return <Doctors />;
-      case 'daycare':
+      case "daycare":
         return <DayCare />;
-      case 'insurance':
+      case "insurance":
         return <Insurance />;
-      case 'orders':
+      case "orders":
         return <Orders />;
       default:
         return null;
@@ -39,14 +96,15 @@ const MainHome = () => {
   return (
     <>
       {/* <Sidebar handleSelection={handleSidebarSelection} /> */}
-      <div style={{ height: '100%' }} className="container-fluid">
-        <div style={{ height: '100%' }} className="row">
-          <div style={{ width: '16%' }}>
-            <Sidebar handleSelection={handleSidebarSelection} />
-          </div>
-          <div style={{ width: '84%', background: '#f9fafe' }}>
-            <div className="content">{renderSelectedComponent()}</div>
-          </div>
+      <div
+        style={{ height: "100%", display: "flex" }}
+        className="container-fluid"
+      >
+        <div style={{ width: "16%", position: "sticky", top: 0 }}>
+          <Sidebar handleSelection={handleSidebarSelection} />
+        </div>
+        <div style={{ width: "84%", background: "#f9fafe", overflow: "auto" }}>
+          <div className="content">{renderSelectedComponent()}</div>
         </div>
       </div>
     </>
