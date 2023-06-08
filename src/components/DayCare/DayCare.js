32b18de179ef50a17179a28999ae36d2eae20cf7
GoharHayyat <goharchisthi@gmail.com>
2023-06-08 22:11:46 +0500

    bb

--- src/components/DayCare/DayCare.js
+++ src/components/DayCare/DayCare.js
@@ -8,27 +8,49 @@ import axios from "axios";
 
 export default function DayCare() {
   const [daycareData, setDaycareData] = useState([]);
+  const [daycare, setDaycare] = useState();
+
+  const toEdit = (ID, edit, val) => {
+    axios
+      .get(`http://localhost:5000/api/daycare/${ID}`)
+      .then((response) => {
+        console.log(response.data);
+        setDaycare(response.data);
+        setDaycare((prevState) => ({
+          ...prevState,
+          [edit]: val,
+        }));
+        if (daycare) {
+          axios
+            .put(`http://localhost:5000/api/daycare/${ID}`, daycare)
+            .then(setDaycare(null));
+        }
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:5000/api/daycare");
         setDaycareData(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("Error fetching daycare data:", error);
       }
     };
-
     fetchData();
   }, []);
 
-  console.log(daycareData);
+  // console.log(daycareData);
 
   return (
     <div>
-      {daycareData.map((entry, index) => (
+      {daycareData.map((entry) => (
         <div
-          key={index}
+          key={entry._id}
           style={{ width: "90%", marginTop: "6%", marginLeft: "5%" }}
         >
           <Accordion disabled={entry.approvedcheck}>
@@ -40,11 +62,7 @@ export default function DayCare() {
               <Typography>{entry.pickupadress}</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
+              <Typography>{/* ?? add button */}</Typography>
             </AccordionDetails>
           </Accordion>
 
