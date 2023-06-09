b0ee93a20027f56c3decc4dd67657f02359f26db
GoharHayyat <goharchisthi@gmail.com>
2023-06-10 01:29:13 +0500

    daycare

--- src/components/DayCare/DayCare.js
+++ src/components/DayCare/DayCare.js
@@ -14,12 +14,50 @@ import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import TableHead from "@mui/material/TableHead";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Box from "@mui/joy/Box";
+import Divider from "@mui/joy/Divider";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 
 export default function DayCare() {
   const [daycareData, setDaycareData] = useState([]);
   const [petinfo, setPetinfo] = useState([]);
   const [triger, setTriger] = useState(null);
   const [daycare, setDaycare] = useState();
+  const [petdetail, setPetdetail] = useState(null);
+
+  const [open, setOpen] = React.useState(false);
+
+  const [filterOption, setFilterOption] = useState("all");
+
+  const filteredData = daycareData.filter((entry) => {
+    if (filterOption === "all") {
+      return !entry.delivered;
+    } else if (filterOption === "pending") {
+      return entry.approvedcheck === false;
+    } else if (filterOption === "drop") {
+      return entry.dropoffstatus === true && entry.delivered === false;
+    } else if (filterOption === "rejected") {
+      return entry.requestrejected === true;
+    } else if (filterOption === "comp") {
+      return entry.delivered === true;
+    }
+    return true;
+  });
+
+  const handlePetdetails = (pets) => {
+    setPetdetail(pets);
+    setOpen(true);
+    console.log(petdetail);
+  };
+  const handleModalClose = () => {
+    setPetdetail(null);
+    setOpen(false);
+  };
 
   const toEdit = (ID, edit, val) => {
     if (edit === "approvedcheck" && val) {
@@ -35,7 +73,7 @@ export default function DayCare() {
           axios
             .put(`http://localhost:5000/api/daycare/${ID}`, updatedData)
             .then(() => {
-              console.log("Daycare updated successfully");
+              toast.success("Sucessfully updated");
               setDaycare(null);
               setTriger("true");
               console.log(triger);
@@ -62,7 +100,7 @@ export default function DayCare() {
       axios
         .put(`http://localhost:5000/api/daycare/${ID}`, updatedData)
         .then(() => {
-          console.log("Daycare updated successfully");
+          toast.success("Sucessfully updated");
           setDaycare(null);
           setTriger("true");
         })
@@ -73,9 +111,8 @@ export default function DayCare() {
       axios
         .get(`http://localhost:5000/api/daycare/${ID}`)
         .then((response) => {
-          console.log(response.data);
           response.data[edit] = val;
-          console.log(response.data);
+          toast.success("Sucessfully updated");
           setDaycare(response.data);
         })
         .catch((error) => {
@@ -83,20 +120,6 @@ export default function DayCare() {
         });
     }
   };
-
-  // const toEdit = (ID, edit, val) => {
-  //   axios
-  //     .get(`http://localhost:5000/api/daycare/${ID}`)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       response.data[edit] = val;
-  //       console.log(response.data);
-  //       setDaycare(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error retrieving data:", error);
-  //     });
-  // };
 
   useEffect(() => {
     if (daycare) {
@@ -132,13 +155,27 @@ export default function DayCare() {
     fetchData();
     if (triger) {
       setTriger(null);
-      //close function here
     }
   }, [triger]);
 
-  const filteredData = daycareData.filter((entry) => !entry.requestrejected);
   return (
-    <div>
+    <div style={{ marginTop: "2%", marginLeft: "10px" }}>
+      <ToastContainer />
+      <div>
+        <span>Filters</span>
+        <select
+          value={filterOption}
+          onChange={(e) => setFilterOption(e.target.value)}
+        >
+          <option value="all">All Requests</option>
+          <option value="pending">Pending Requests</option>
+
+          <option value="drop">Dropoff Requests</option>
+
+          <option value="rejected">Rejected Requests</option>
+          <option value="comp">completed Requests</option>
+        </select>
+      </div>
       {filteredData.length === 0 ? (
         <div className="no-requests">
           <p>No requests at this time.</p>
@@ -152,14 +189,7 @@ export default function DayCare() {
               key={entry._id}
               style={{ width: "90%", marginTop: "6%", marginLeft: "5%" }}
             >
-              <Typography>
-                Daycare Request Details ID: {entry._id}{" "}
-                <Chip
-                  label=" Details"
-                  variant="outlined"
-                  style={{ backgroundColor: "grey" }}
-                />
-              </Typography>
+              <Typography>Daycare Request Details ID: {entry._id} </Typography>
               <Accordion disabled={entry.approvedcheck}>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
@@ -209,6 +239,7 @@ export default function DayCare() {
                               label="View Pet Details"
                               variant="outlined"
                               style={{ backgroundColor: "grey" }}
+                              onClick={() => handlePetdetails(pet)}
                             />
                           </Typography>
                         </TableCell>
@@ -347,8 +378,51 @@ export default function DayCare() {
                   aria-controls="panel3a-content"
                   id="panel3a-header"
                 >
-                  <Typography>Accordion 3</Typography>
+                  <Typography>Step 3: Status</Typography>
                 </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell align="left">Pickedup date</TableCell>
+                            <TableCell align="right">Identity Code</TableCell>
+                            <TableCell align="right">Days Requested </TableCell>
+                            <TableCell align="right">Status </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          <TableRow
+                            sx={{
+                              "&:last-child td, &:last-child th": {
+                                border: 0,
+                              },
+                            }}
+                          >
+                            <TableCell align="left">
+                              <Typography>{entry.pickupdate}</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              {entry.identity_number}
+                            </TableCell>
+                            <TableCell align="right">
+                              <Typography>
+                                {entry.toaldays === "1" && <div>1 day</div>}
+                                {entry.toaldays === "2" && <div>2 days</div>}
+                                {entry.toaldays === "3" && <div>3 days</div>}
+                                {entry.toaldays === "4" && <div>Notsure</div>}
+                              </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              <span>Pet is being taken care</span>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Typography>
+                </AccordionDetails>
               </Accordion>
 
               <Accordion disabled={entry.delivered || !entry.dropoffstatus}>
@@ -357,13 +431,192 @@ export default function DayCare() {
                   aria-controls="panel3a-content"
                   id="panel3a-header"
                 >
-                  <Typography>Accordion 4</Typography>
+                  <Typography>Step 4: dropoff and Payment </Typography>
                 </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell align="left">Dropoff date</TableCell>
+                            <TableCell align="left">Dropoff Adress</TableCell>
+                            <TableCell align="left">Dropoff city</TableCell>
+                            <TableCell align="left">Dropoff time</TableCell>
+                            <TableCell align="right">Days serviced </TableCell>
+                            <TableCell align="right">Payment amount </TableCell>
+                            <TableCell align="right">Status </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          <TableRow
+                            sx={{
+                              "&:last-child td, &:last-child th": {
+                                border: 0,
+                              },
+                            }}
+                          >
+                            <TableCell align="left">
+                              <Typography>{entry.dropoffdate}</Typography>
+                            </TableCell>
+                            <TableCell align="left">
+                              <Typography>{entry.dropoffadress}</Typography>
+                            </TableCell>
+
+                            <TableCell align="left">
+                              <Typography>{entry.dropoffcity}</Typography>
+                            </TableCell>
+
+                            <TableCell align="left">
+                              <Typography>
+                                {entry.dropofftime === "1" && (
+                                  <div>09:00am-10:00am</div>
+                                )}
+                                {entry.dropofftime === "2" && (
+                                  <div>10:00am-11:00am</div>
+                                )}
+                                {entry.dropofftime === "3" && (
+                                  <div>11:00am-12:00pm</div>
+                                )}
+                                {entry.dropofftime === "4" && (
+                                  <div>12:00pm-01:00pm</div>
+                                )}
+                                {entry.dropofftime === "5" && (
+                                  <div>01:00pm-02:00pm</div>
+                                )}
+                                {entry.dropofftime === "6" && (
+                                  <div>02:00pm-03:00pm</div>
+                                )}
+                                {entry.dropofftime === "7" && (
+                                  <div>03:00pm-04:00pm</div>
+                                )}
+                                {entry.dropofftime === "8" && (
+                                  <div>04:00pm-05:00pm</div>
+                                )}
+                                {entry.dropofftime === "9" && (
+                                  <div>05:00pm-06:00pm</div>
+                                )}
+                              </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              <Typography>{entry.daysservise}</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              <Typography>{entry.bill}.Rs</Typography>
+                            </TableCell>
+
+                            <TableCell align="right">
+                              <Typography>
+                                <Chip
+                                  label="Pet Dropedoff"
+                                  variant="outlined"
+                                  onClick={() => {
+                                    toEdit(entry._id, "delivered", true);
+                                  }}
+                                  style={{ backgroundColor: "#63F263" }}
+                                />
+                              </Typography>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Typography>
+                </AccordionDetails>
               </Accordion>
             </div>
           );
         })
       )}
+      <React.Fragment>
+        <Modal open={open} onClose={handleModalClose}>
+          <ModalDialog
+            variant="outlined"
+            role="alertdialog"
+            aria-labelledby="alert-dialog-modal-title"
+            aria-describedby="alert-dialog-modal-description"
+          >
+            <Typography
+              style={{ fontSize: "30px" }}
+              id="alert-dialog-modal-title"
+              component="h1"
+              startDecorator={<WarningRoundedIcon />}
+            >
+              Details
+            </Typography>
+            <Divider />
+            {petdetail && (
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+              >
+                <div style={{ fontSize: "20px", display: "flex" }}>
+                  pet type:
+                  <div style={{ color: "grey" }}>{petdetail.pet}</div>
+                </div>
+              </Typography>
+            )}
+
+            {petdetail && (
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+              >
+                <div style={{ fontSize: "20px", display: "flex" }}>
+                  breed:
+                  <div style={{ color: "grey" }}>{petdetail.breed}</div>
+                </div>
+              </Typography>
+            )}
+
+            {petdetail && (
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+              >
+                <div style={{ fontSize: "20px", display: "flex" }}>
+                  Gender:
+                  <div style={{ color: "grey" }}>{petdetail.gender}</div>
+                </div>
+              </Typography>
+            )}
+
+            {petdetail && (
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+              >
+                <div style={{ fontSize: "20px", display: "flex" }}>
+                  weight:
+                  <div style={{ color: "grey" }}>{petdetail.weight}</div>
+                </div>
+              </Typography>
+            )}
+            {petdetail && (
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+              >
+                <div style={{ fontSize: "20px", display: "flex" }}>
+                  Age:
+                  <div style={{ display: "flex", color: "grey" }}>
+                    {petdetail.years}year {petdetail.months}month
+                  </div>
+                </div>
+              </Typography>
+            )}
+
+            <Box
+              sx={{
+                display: "flex",
+                gap: 1,
+                justifyContent: "flex-end",
+                pt: 2,
+              }}
+            ></Box>
+          </ModalDialog>
+        </Modal>
+      </React.Fragment>
     </div>
   );
 }
