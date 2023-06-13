9a00742a3445d3e116bfc70c2a2a5c25b0db1737
TheBaloch <abdullahbaloch797@gmail.com>
2023-06-13 05:17:52 +0500

    fixed something

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -4,14 +4,12 @@ export default function User(user) {
   console.log(user);
   const u = user.user;
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <p className="card-text">First Name: {u.f_name}</p>
-          <p className="card-text">Last Name: {u.l_name}</p>
-          <p className="card-text">Email: {u.email}</p>
-          <p className="card-text">Phone: {u.phone}</p>
-        </div>
+    <div className="card">
+      <div className="card-body">
+        <p className="card-text">First Name: {u.f_name}</p>
+        <p className="card-text">Last Name: {u.l_name}</p>
+        <p className="card-text">Email: {u.email}</p>
+        <p className="card-text">Phone: {u.phone}</p>
       </div>
     </div>
   );
