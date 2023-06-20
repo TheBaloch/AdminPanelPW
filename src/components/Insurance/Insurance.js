d4bb011a2e98ef827795e644992e6fdc160b8dfb
TheBaloch <abdullahbaloch797@gmail.com>
2023-06-20 23:16:58 +0500

    ..

--- src/components/Insurance/Insurance.js
+++ src/components/Insurance/Insurance.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Insurance() {
-  return <div>Insurance</div>;
+  return (
+    <>
+      <button className="btn btn-primary">Add new</button>
+      <div>Insurance</div>
+    </>
+  );
 }
